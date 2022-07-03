0c5b6f99bd636c6f28889e277f0393077eb335f4
yeonsung Joe <jjo3ys@gmail.com>
2022-07-03 19:55:27 +0900

    <feat>Multi Linear Regression

--- data_augmentation.py
+++ data_augmentation.py
@@ -17,8 +17,8 @@ buildings = ['공과대학',
              '정보전산원', 
              '컨벤션센터', 
              '학생복지회관']
-             
-def DataAugumentaion():
+
+def Supply_Return_DataAugumentaion():
     datetime_id = pd.date_range(start='2021-06-01 00:00:00', end='2021-06-30 23:45:00', freq="15T")
     df = pd.DataFrame(index=datetime_id)
 
@@ -50,3 +50,49 @@ def DataAugumentaion():
         df["{}_return".format(building)] = return_list
 
     df.to_csv('augumentationed_data.csv')
+
+def Degree_DataAugumentaion():
+    df = pd.read_csv('외기온도.csv', index_col=None)
+    degree = df['온도'].to_numpy().tolist()
+    for i in range(len(degree)):
+        if str(degree[i]) == 'nan' and str(degree[i+1]) == 'nan':
+            count = 2
+            while True:
+                if str(degree[i+count]) != 'nan':break
+                count += 1
+            try:
+                degree[i] = (degree[i-1]+degree[i+count])/count
+            except:
+                degree[i] = sum(degree)/len(degree)
+        
+        elif str(degree[i]) == 'nan':
+            try:
+                ave = (degree[i-1]+degree[i+1])/2
+                degree[i] = int(ave)
+            except:
+                degree[i] = sum(degree)/len(degree)
+
+    df['온도'] = degree
+    df.to_csv('외기온도.csv', columns=['날짜', '온도'], index=None)
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+df = pd.read_csv("augumentationed_data.csv", index_col=None)
+y = pd.read_csv('외기온도.csv', index_col=None)['온도']
+x = df.drop('날짜', axis=1)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8, test_size=0.2)
+MLR = LinearRegression()
+MLR.fit(x_train, y_train)
+
+y_predict = MLR.predict(x_test)
+
+import matplotlib.pyplot as plt
+plt.scatter(y_test, y_predict, alpha=0.4)
+plt.xlabel("Actual Rent")
+plt.ylabel("Predicted Rent")
+plt.title("MULTIPLE LINEAR REGRESSION")
+plt.savefig('Result.png')
+
+print("Accuracy : {}%".format(round(1-((y_test-y_predict)**2).sum()/(((y - y.mean()) ** 2).sum()), 4)*100))
