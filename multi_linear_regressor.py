7557e061a0e0a87ff6b07c1eb7afc335db6faa1e
yeonsung Joe <jjo3ys@gmail.com>
2022-07-04 16:37:51 +0900

    <feat> make result csv

--- multi_linear_regressor.py
+++ multi_linear_regressor.py
@@ -6,20 +6,23 @@ from sklearn.linear_model import LinearRegression
 
 df1 = pd.read_csv("augumentationed_data.csv", index_col=None)
 
-def return_degree_model():
+def return_degree_model(save=False):
     df2 = pd.read_csv("degree_data.csv", index_col=None)
 
     df = pd.merge(df1, df2, how='left')
 
-    x = df.drop(['날짜', '환수온도'], axis=1)
+    x = df.drop('환수온도', axis=1)
     y = df['환수온도']
 
     x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8, test_size=0.2)
 
-    MLR = LinearRegression()
-    MLR.fit(x_train, y_train)
+    x_train1 = x_train.drop('날짜', axis=1)
+    x_test1 = x_test.drop('날짜', axis=1)
 
-    y_predict = MLR.predict(x_test)
+    MLR = LinearRegression()
+    MLR.fit(x_train1, y_train)
+
+    y_predict = MLR.predict(x_test1)
 
     plt.scatter(y_test, y_predict, alpha=0.4)
     plt.xlabel("Actual Return Degree")
@@ -27,9 +30,21 @@ def return_degree_model():
     plt.title("MULTIPLE LINEAR REGRESSION")
     plt.savefig('Return_Degree_Result.png')
 
-    print('Accuracy : {}%'.format(round(1-(((y_test-y_predict)**2).sum()/((y-y.mean())**2).sum()), 4)*100))
+    print('R**2 : {}%'.format(round(1-(((y_test-y_predict)**2).sum()/((y-y.mean())**2).sum()), 4)*100))
 
-def temperature_model():
+    if save:
+        result_df = pd.DataFrame()
+        for x in x_test.columns:
+            result_df[x] = x_test[x]
+        
+        result_df['real_return'] = y_test
+        result_df['pred_return'] = y_predict
+
+        result_df = result_df.sort_index()
+
+        result_df.to_csv("Return_Model_Result.csv", index=None)
+
+def temperature_model(save=False):
     df2 = pd.read_csv("temperature.csv", index_col=None)
 
     df = pd.merge(df1, df2, how='left')
@@ -43,11 +58,23 @@ def temperature_model():
     MLR.fit(x_train, y_train)
 
     y_predict = MLR.predict(x_test)
-
+    
     plt.scatter(y_test, y_predict, alpha=0.4)
     plt.xlabel("Actual Temperature")
     plt.ylabel("Predicted Temperature")
     plt.title("MULTIPLE LINEAR REGRESSION")
     plt.savefig('Temperature_Result.png')
 
-    print('Accuracy : {}%'.format(round(1-(((y_test-y_predict)**2).sum()/((y-y.mean())**2).sum()), 4)*100))
+    print('R**2 : {}%'.format(round(1-(((y_test-y_predict)**2).sum()/((y-y.mean())**2).sum()), 4)*100))
+
+    if save:
+        result_df = pd.DataFrame()
+        for x in x_test.columns:
+            result_df[x] = x_test[x]
+        
+        result_df['real_temp'] = y_test
+        result_df['pred_temp'] = y_predict
+
+        result_df.to_csv("Temp_Model_Result.csv", index=None)
+
+return_degree_model(save=True)
